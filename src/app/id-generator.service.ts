f2ed3d45d369349ac3b10eeb22321c0abdb74547
Константин Закиров <kostazakirov30@gmail.com>
2024-08-21 19:45:41 +0300

    Update id-generator.service.ts

--- src/app/id-generator.service.ts
+++ src/app/id-generator.service.ts
@@ -1,11 +1,12 @@
+import { Injectable } from '@angular/core';
 import { IdGenerator } from './id-generator';
 
+@Injectable()
 export class IdGeneratorService extends IdGenerator {
-    private currentId: number = 0;
-  
-    generate(): number {
-      this.currentId += 1;
-      return this.currentId;
-    }
+  private currentId: number = 0;
+
+  generate(): number {
+    this.currentId += 1;
+    return this.currentId;
   }
-  
+}
